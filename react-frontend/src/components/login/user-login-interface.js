54b81d70b6b8276a78c9edf02479c6e0a34ebd13
Nicky Sitnikovs <nickystnkvs@outlook.com>
2020-12-14 22:22:00 +0000

    Landing page changes

--- react-frontend/src/components/login/user-login-interface.js
+++ react-frontend/src/components/login/user-login-interface.js
@@ -1,24 +1,98 @@
 import {LoginRegisterSelector} from "./login-register-selector";
 import "../../static/css/login/user-login-interface.css";
-import {Button} from "react-bootstrap";
+import {animated, useSpring, config} from "react-spring";
+import {useState} from "react";
+import greenIcon from '../../static/images/green.png';
+import orangeIcon from '../../static/images/orange.png';
+import pinkIcon from '../../static/images/pink.png';
 
 export const UserLoginInterface = (props) => {
+    const slideInAnimation = useSpring({
+        from: {height: 0, opacity: 0, color: "#051C2C", fontSize: "0em"},
+        to: {height: 100, opacity: 1, color: "#FF8D72", fontSize: "1em"},
+        config: {
+
+            clamp: true,
+            config: config.stiff
+        },
+        delay: 400
+    }) // todo: would it be possible to add an animated underline?
+
+    const popUpAnimation = useSpring({
+        from: {fontSize: "0px"},
+        to: {fontSize: "16px"},
+        delay: 1000,
+        config: {
+            clamp: true,
+            config: config.stiff
+        }
+    })
+
+    const buttonAnimation = useSpring({
+        from: {color: "#FFFFFF", borderColor: "#FFFFFF"},
+        to: {color: "#FF8D72", backgroundColor: "#FF8D72", borderColor: "#FF8D72"},
+        delay: 1500,
+        config: {
+            clamp: true,
+            config: config.stiff
+        }
+    })
     return(
         <div className="container-fluid landing-page-container">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6" style={{"width": "max-content"}}>
+            <div className="container my-auto h-100">
+                <div className="row h-100">
+                    <div className="col-md-6 introduction my-auto" style={{"width": "max-content"}}>
                         <h1 className="title">
                             Your notes.
                             <br />
-                            <strong className="standout-text">Revolutionized.</strong>
+                            <animated.strong style={slideInAnimation} className="standout-text">Revolutionized.</animated.strong>
                         </h1>
-                        <p className="description" style={{"color": "#ced4da"}}>
+                        <animated.p style = {popUpAnimation} className="description">
                             Dark themed notebook filled with features.
                             <br />
                             Blazingly fast.
-                        </p>
-                        <button className="btn see-more">See what it's about</button>
+                        </animated.p>
+
+                        <animated.button
+                            style={buttonAnimation}
+                            className="btn see-more center-block"
+                        >
+                            See more
+                        </animated.button>
+                    </div>
+
+                    <div className="col-md-6 features">
+                        {/* Since this is static, we can make it into an object and then .map() */}
+                        <div className="feature">
+                            <h4 className="feature-title"> Offline notes</h4>
+                            <div className="icon-container">
+                                <img src={greenIcon} className="feature-icon" alt="By downloading your notes, you can read and edit them offline."/>
+                            </div>
+                            <p className="description">
+                                Download your notes to view and continue writing offline.
+                            </p>
+                        </div>
+                        <div className="feature pt-5">
+                            <h4 className="feature-title">Elegant</h4>
+                            <div className="icon-container">
+                                <img src={orangeIcon} className="feature-icon" alt="By downloading your notes, you can read and edit them offline."/>
+                            </div>
+                            <p className="description">
+                                Our soothing dark theme is easy on the eyes.
+                            </p>
+                        </div>
+                        <div className="feature pt-5">
+                            <h4 className="feature-title">
+                                Fast
+                            </h4>
+                            <div className="icon-container">
+                                <img src={pinkIcon} className="feature-icon" alt="By downloading your notes, you can read and edit them offline."/>
+                            </div>
+
+                            <p className="description">
+                                Simple and concise, our notebook allows you get stuff done. Quickly.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
