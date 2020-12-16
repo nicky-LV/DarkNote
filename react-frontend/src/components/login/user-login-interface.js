b35c188da74012dc2a9b930a4e97509b9fbfe0b2
Nicky Sitnikovs <nickystnkvs@outlook.com>
2020-12-16 16:42:20 +0000

    .see-more button animation w/ ref

--- react-frontend/src/components/login/user-login-interface.js
+++ react-frontend/src/components/login/user-login-interface.js
@@ -1,12 +1,23 @@
 import {LoginRegisterSelector} from "./login-register-selector";
 import "../../static/css/login/user-login-interface.css";
 import {animated, useSpring, config} from "react-spring";
-import {useState} from "react";
+import {useState, useRef, useLayoutEffect} from "react";
 import greenIcon from '../../static/images/green.png';
 import orangeIcon from '../../static/images/orange.png';
 import pinkIcon from '../../static/images/pink.png';
 
 export const UserLoginInterface = (props) => {
+    const [descriptionWidth, setDescriptionWidth] = useState(null)
+    const descriptionRef = useRef(); // allows us to access properties of the "description div" element in the DOM
+
+    useLayoutEffect(() =>{
+        const descriptionWidth = descriptionRef.current.clientWidth
+        setDescriptionWidth(descriptionWidth)
+    }, [])
+
+    // useLayoutEffect should be used when working with the DOM. useEffect will run after content is shown to the user (which can be unpleasant).
+    // useLayoutEffect handles your DOM operations before actually rendering to the user. Advantage.
+
     const slideInAnimation = useSpring({
         from: {height: 0, opacity: 0, color: "#051C2C", fontSize: "0em"},
         to: {height: 100, opacity: 1, color: "#FF8D72", fontSize: "1em"},
@@ -29,8 +40,8 @@ export const UserLoginInterface = (props) => {
     })
 
     const buttonAnimation = useSpring({
-        from: {color: "#FFFFFF", borderColor: "#FFFFFF"},
-        to: {color: "#FF8D72", backgroundColor: "#FF8D72", borderColor: "#FF8D72"},
+        from: {width: 0, opacity: 0},
+        to: {width: descriptionWidth, opacity: 1},
         delay: 1500,
         config: {
             clamp: true,
@@ -53,7 +64,7 @@ export const UserLoginInterface = (props) => {
                             <br />
                             <animated.strong style={slideInAnimation} className="standout-text">Revolutionized.</animated.strong>
                         </h1>
-                        <animated.p style = {popUpAnimation} className="description">
+                        <animated.p style = {popUpAnimation} className="description" ref={descriptionRef}>
                             Dark themed notebook filled with features.
                             <br />
                             Blazingly fast.
