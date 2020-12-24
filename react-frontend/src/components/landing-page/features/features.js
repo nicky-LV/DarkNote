9341b01bcb5679634a3fb58250b3a25dea860a4e
= <nickystnkvs@outlook.com>
2020-12-24 23:47:16 +0000

    Added animated logo, setup login section on landing page

--- react-frontend/src/components/landing-page/features/features.js
+++ react-frontend/src/components/landing-page/features/features.js
@@ -1,8 +1,6 @@
-import greenIcon from "../../../static/images/green.png";
-import orangeIcon from "../../../static/images/orange.png";
-import pinkIcon from "../../../static/images/pink.png";
 import {animated, useSpring} from "react-spring";
 import './features.css';
+import React from "react";
 
 export const Features = (props) => {
     const featuresAnimation = useSpring({
@@ -11,33 +9,25 @@ export const Features = (props) => {
         delay: 800
     })
     return (
-        <animated.div className="features my-auto" style={featuresAnimation}>
+        <animated.div className="features my-auto px-0" style={featuresAnimation}>
+            <h1 className="features-header mb-5">Features</h1>
             {/* Since this is static, we can make it into an object and then .map() */}
             <div className="feature">
                 <h4 className="feature-title"> Offline notes </h4>
-                <div className="icon-container">
-                    <img src={greenIcon} className="feature-icon" alt="By downloading your notes, you can read and edit them offline."/>
-                </div>
                 <p className="description">
                     Download your notes to view and continue writing offline.
                 </p>
             </div>
-            <div className="feature pt-5">
+            <div className="feature mt-5">
                 <h4 className="feature-title"> Elegant </h4>
-                <div className="icon-container">
-                    <img src={orangeIcon} className="feature-icon" alt="By downloading your notes, you can read and edit them offline."/>
-                </div>
                 <p className="description">
                     Our soothing dark theme is easy on the eyes.
                 </p>
             </div>
-            <div className="feature pt-5">
+            <div className="feature mt-5">
                 <h4 className="feature-title">
                     Fast
                 </h4>
-                <div className="icon-container">
-                    <img src={pinkIcon} className="feature-icon" alt="By downloading your notes, you can read and edit them offline."/>
-                </div>
 
                 <p className="description">
                     Simple and concise, our notebook allows you get stuff done. Quickly.
--- react-frontend/src/components/landing-page/landing-page.js
+++ react-frontend/src/components/landing-page/landing-page.js
@@ -1,8 +1,10 @@
-import "../../static/css/login/user-login-interface.css";
+import "./landing-page.css";
 import {Features} from "./features/features";
 import {Introduction} from "./introduction/introduction";
-import {UserLogin} from "./user-login";
 import notesIllustration from '../../static/images/undraw_on_the_office_fbfs.svg';
+import Logo from "../../static/images/Logo.svg";
+import Arrow from "../../static/images/Arrow.svg";
+import React from "react";
 
 export const LandingPage = (props) => {
 
@@ -10,17 +12,25 @@ export const LandingPage = (props) => {
         <div className="container-fluid landing-page-container">
             <div className="container my-auto h-100">
                 <div className="row">
-                    Navbar here
+                    <img className="position-absolute" src={Logo} alt="DarkNote logo" height="200px" width="250px"/>
                 </div>
-            <div className="row h-100">
+            <div className="row vh-100">
                 <Introduction />
                 <div className="col-md-6 my-auto">
                     <img src={notesIllustration} alt="Person managing their notes" className="using-notes-icon"/>
                 </div>
             </div>
-            <div className="row h-100 login-section">
-                <UserLogin />
+            <div className="row h-100 p-0 features-section">
+                <div className="col-md">
+                    <Features/>
+                </div>
+                <div className="col-md">
+                    <img src={Arrow} alt="Arrow pointing to login section" className="arrow" />
+                </div>
             </div>
+
+                <div className="row login-section">
+                </div>
         </div>
 </div>)
 
